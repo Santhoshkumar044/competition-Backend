839fa04a44d4e8f2e9a1676bbd09abb7c532e3b6
Santhoshkumar044 <santhoshkumar.vt9a@gmail.com>
2025-05-20 09:46:19 +0530

    git repo initializied

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("Server is ready");
+})
+
+const PORT = process.env.PORT || 5000; 
+ console.log(process.env.MONGO_URI);
+ app.listen(PORT,() =>{
+    connectDB();
+    console.log(`Server is running in ${PORT}`);
+ })
