2a6f1dae9eb3449870eb9b7c6c39702faec8edd7
Santhoshkumar044 <santhoshkumar.vt9a@gmail.com>
2025-05-21 09:01:57 +0530

    Update users.js

--- models/users.js
+++ models/users.js
@@ -3,9 +3,8 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true},
-  password: { type: String, required: true },
   role: { type: String, enum: ['student', 'host'], default: null },
-  RegisterNo :{type:String,unique:true},
+  RegisterNo :{type:String,unique:true,sparse : true,required : false},
   department: { type: String },
   yearOfStudy: { type: String },
   collegeName: { type: String },
