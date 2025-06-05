2c7b4b6d22e2a54c4d9dc01b8710e5d66fe60e52
Santhoshkumar044 <santhoshkumar.vt9a@gmail.com>
2025-06-05 12:36:29 +0530

    Cleared days left bug

--- scrapers/unstopScraper.js
+++ scrapers/unstopScraper.js
@@ -31,13 +31,20 @@ export const scrapeUnstop = async () => {
     cards.forEach(card => {
       const title = card.querySelector('h2')?.innerText?.trim() || '';
       const location = card.querySelector('p')?.innerText?.trim() || '';
-      const daysLeft = card.querySelector('div.seperate_box.align-center')?.textContent.trim().replace(/\s+/g, ' ');
+
+      const timingDivs = Array.from(card.querySelectorAll('div.seperate_box.align-center'));
+      const daysLeftDiv = timingDivs.find(div => {
+        const span = div.querySelector('span');
+        return span && span.innerText.toLowerCase().includes('days left');
+      });
+      const daysLeft = daysLeftDiv ? daysLeftDiv.textContent.trim().replace(/\s+/g, ' ') : '';
+
       const prize = card.querySelector('div.seperate_box.prize')?.innerText.trim() || 'Non cash prize';
       const organiser = card.querySelector('.content p')?.innerText.trim() || 'Unstop';
       const id = card.getAttribute('id')?.split('_')[1];
       const link = id ? `https://unstop.com/hackathon/${title.toLowerCase().replace(/\s+/g, '-')}-${id}` : '';
 
-      results.push({ title, location, organiser, prize, daysLeft, link});
+      results.push({ title, location, organiser, prize, daysLeft, link });
     });
 
     return results;
