aa669a27807e52c5207318beee38d8d94c168041
Santhoshkumar044 <santhoshkumar.vt9a@gmail.com>
2025-06-06 08:38:20 +0530

    event schema added

--- models/eventschema.js
+++ models/eventschema.js
@@ -3,11 +3,17 @@ const eventSchema = new mongoose.Schema({
   title: {type: String,required: true},
   description:{type: String},
   collegeName:{type: String},
-  venue:{type: String},
-  startDateTime:{type: Date},
-  endDateTime:{type: Date},
+  venue:{
+    roomNumber:String,
+    location: String,
+    capacity: Number,
+  },
+  EventDate:{type:Date},
+  startTime:{type:String},
+  endTime:{type: String},
   isRegistrationOpen:{type: Boolean,default: true},
 
 }, { timestamps: true });
 
-export default mongoose.model('Event', eventSchema);
+const Event = mongoose.model('Event', eventSchema);
+export default Event;
